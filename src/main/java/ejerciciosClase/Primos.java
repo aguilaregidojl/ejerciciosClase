ab4aea823b66d62be18312dc90fa1b44118569d1
java <java@DESKTOP-0AUP1R3>
2023-05-18 15:23:26 +0200

    Actualización 1

---
+++ src/main/java/ejerciciosClase/Primos.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejerciciosClase;
+
+import java.util.Scanner;
+
+/**
+ * Programa para calcular si un número es o no es un número primo.
+ * 
+ * @author Jose Luis Aguilar Egido
+ */
+
+public class Primos {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int x;
+        int resto;
+        int contador;
+        int divisor;
+        System.out.println("Introduce el número a comprobar :");
+        x = teclado.nextInt();
+        divisor = x;
+        contador = 0;
+
+        while (divisor > 0) {
+            resto = (x % divisor);
+            if (resto == 0) {
+                contador++;
+                divisor--;
+            } else {
+                divisor--;
+            }
+        }
+        if (contador == 2) {
+            System.out.println("Es primo.");
+        } else {
+            System.out.println("No es primo");
+        }
+    }
+}
---
+++ src/main/java/ejerciciosClase/sumatorio.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejerciciosClase;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author java
+ */
+public class sumatorio {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int num1, num2, suma;
+        float promedio;
+        System.out.println("Ingrese primer valor:");
+        num1 = teclado.nextInt();
+        System.out.println("Ingrese segundo valor");
+        num2 = teclado.nextInt();
+        suma = num1 + num2;
+        promedio = ((float) num1 + (float) num2) / 2;
+        System.out.print("La suma de los dos valores es:");
+        System.out.println(suma);
+        System.out.print("El promedio de los dos valores es:");
+        System.out.println(promedio);
+    }
+
+}
