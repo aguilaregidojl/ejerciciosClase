7e5de78d8144f9f03ad9906eed96e5b2726aa6ff
java <java@DESKTOP-0AUP1R3>
2023-05-22 18:22:14 +0200

    Resubida 2

---
+++ src/main/java/General/EjercicioBucles.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package General;
+
+/**
+ *
+ * @author java
+ */
+public class EjercicioBucles {
+    public static void main(String[] args) {
+        //Ejercicio con WHILE
+        
+        /*int numero=0;
+        
+        System.out.println("Los múltiplos de 5 entre 0 y 100 son:");
+        while (numero<100) {
+            numero=numero+5;
+            System.out.print(numero + " ");
+        }*/
+        
+        // Ejercicio con FOR
+       
+        /*System.out.println("Los múltiplos de 5 entre 0 y 100 son:");
+        for (int numero=5; numero<100; numero=numero+5) {
+            System.out.print(numero + " ");
+        }*/
+        
+        //Ejercicio con DO WHILE
+        /*int numero=0;
+        System.out.println("Los múltiplos de 5 entre 0 y 100 son:");
+        do {
+            numero=numero+5;
+            System.out.print(numero + " ");
+        } while (numero<100);*/
+        
+        System.out.println("Entre el 320 y el 160, los números de 20 en 20 son:");
+        for (int numero=320; numero>159; numero=numero-20) {
+            System.out.print(numero + " ");
+        }
+    }
+}
---
+++ src/main/java/General/calculadoraDivisores.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package General;
+
+/**
+ *
+ * @author java
+ */
+public class calculadoraDivisores {
+
+    public static void main(String[] args) {
+        //Inicializamos las variables divisor y número
+        int divisor;
+        int numero = 0;
+        int divsuma= 0;
+
+        //Creamos un bucle que calcule los divisores de la variable número
+        while (numero < 300) {
+            numero++;
+            System.out.print("Número " + numero + ": ");
+            divisor=1;
+            while (divisor != numero) {
+                    if (numero % divisor == 0) {
+                    System.out.print(divisor + " ");
+                    divsuma+=divisor; //Es igual que divsuma=divsuma+divisor
+                }
+                divisor++;
+            }
+            System.out.print(" La suma es: " + divsuma);
+            System.out.println(" ");
+            divsuma=0;
+        }
+    }
+}
