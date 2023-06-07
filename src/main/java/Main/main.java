e645dbb5c994e80a5b8856625c4da38a2ff14f4c
java <java@DESKTOP-0AUP1R3>
2023-06-07 20:37:10 +0200

    Miercoles 20h

---
+++ src/main/java/Main/main.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Main;
+
+import Controlador.controlador;
+import Modelo.clientes;
+import Modelo.conexion;
+import Vista.vistaClientes;
+
+/**
+ *
+ * @author java
+ */
+public class main {
+
+    public static void main(String[] args) {
+        conexion conexion = new conexion();
+        clientes clientes = new clientes();
+        vistaClientes vista = new vistaClientes();
+
+        controlador controlador = new controlador(vista, conexion, clientes);
+        controlador.iniciar();
+        vista.setVisible(true);
+        
+        
+        
+
+    }
+
+}
